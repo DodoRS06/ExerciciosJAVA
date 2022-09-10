09931190108d4106102789616820bf895860644d
DodoRS06 <douglas.raynner0110@gmail.com>
2022-09-09 23:34:01 -0300

    fifteenth commit

--- POO/Exercicio14/app/src/main/java/Exercicio14/Fatura.java
+++ POO/Exercicio14/app/src/main/java/Exercicio14/Fatura.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Exercicio14;
 
-/**
- *
- * @author dodos
- */
 public class Fatura {
+    
+    private String numero, descricao;
+    private int quantidade;
+    private double preco;
+
+    public Fatura(){
+        
+    }
+    
+    public double getTotalFatura(){
+        double total = 0, valor;
+        valor = quantidade * preco;
+        total = total + valor;
+        return total;
+    }
+    
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        if(quantidade < 0){
+            this.quantidade = 0;
+        }
+        else{
+            this.quantidade = quantidade;
+        }
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        if(preco < 0){
+            this.preco = 0;
+        }
+        else{
+            this.preco = preco;
+        }
+    }
+    
     
 }
